5024553e9d63731195e5dc337ce51f3d749bdfe8
Quentin Honnart <quentin.honnart@gmail.com>
2023-03-10 15:02:48 +0100

    link theme.json and tailwind config

--- htdocs/content/themes/portfolio/tailwind.config.js
+++ htdocs/content/themes/portfolio/tailwind.config.js
@@ -1,3 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const fs = require('fs');
+const themeJson = fs.readFileSync('./theme.json');
+const theme = JSON.parse(themeJson);
+const themeColors = theme.settings.color.palette.reduce((acc, item) => {
+  const [color, number] = item.slug.split('-');
+  let colorVariants = [];
+  if (typeof item.variants !== 'undefined' && item.variants === true) {
+    colorVariants = tailwindColorPalette(item.color, {'name': color});
+  } else {
+    colorVariants[color] = item.color;
+  }
+  if (undefined !== number) {
+    if (!acc[color]) {
+      acc[color] = {};
+    }
+    acc[color][number] = colorVariants[color];
+  } else {
+    acc[color] = colorVariants[color];
+  }
+
+  return acc;
+}, {});
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +31,12 @@ module.exports = {
     './views/**/*.blade.php'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: themeColors,
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ],
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('page', function () {
+    return view('pages.default');
+});
