84197a5491ab4ac1489822a70110f76a0c41f0e6
rolpf <jiroxane@hotmail.fr>
2023-05-06 14:59:14 +0200

    post type projet init

--- app/Hooks/Projet.php
+++ app/Hooks/Projet.php
@@ -11,6 +11,20 @@ class Projet extends Hookable
     static $postType = false;
     public function register()
     {
-        self::$postType = PostType::make('projets', 'Projets', 'Projet')->set();
+        self::$postType = PostType::make('projets', 'Projets', 'Projet')
+        ->setLabels([
+            'add_new_item' => __('Ajouter un projet'),
+            'edit_item' => __('Modifier un projet'),
+            'name' => __('Projets'),
+            'search_items' => __('Rechercher un projet'),
+            'singular_name' => __('Projet'),
+        ])
+        ->setArguments([
+            'public' => true,
+            'supports' => ['title', 'excerpt','thumbnail'],
+            'menu_icon' => 'dashicons-portfolio',
+            'show_in_nav_menus' => true,
+        ])
+        ->set();
     }
 }
